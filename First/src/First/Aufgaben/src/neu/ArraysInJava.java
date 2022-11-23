0dd6381ad14906e7547d535e2442359fe8e58345
FS <francesco9721@gmail.com>
2022-11-23 12:40:41 +0100

    Arrays

--- First/src/First/Aufgaben/src/neu/ArraysInJava.java
+++ First/src/First/Aufgaben/src/neu/ArraysInJava.java
@@ -7,6 +7,9 @@ public class ArraysInJava {
 		
 		int [] iNoten = new int [10];
 		String [] strNamen = {"Peter","Hans","Lars"};
+		int [] iWerte;
+		iWerte = new int [10];
+		iWerte[1] = 2;
 		
 		for (int i = 0; i < iNoten.length; i++) {
 			System.out.println(iNoten[i]+ i);
@@ -15,7 +18,17 @@ public class ArraysInJava {
 		for (int i = 0; i < strNamen.length; i++) {
 			System.out.println("Hier ist der " + (i+1) + ". Name: " + strNamen[i]);
 		}
+
+		// Werte nachzählen
+		for (int i = 0; i < iWerte.length; i++) {
+			System.out.println(i);
+		}
 		
+		
+		// Werte auslesen 
+		for (int i : iWerte) {
+			System.out.println(i);
+		}
 	
 	}
 
