66227de6a301621a0d123d6874ec335854cf7343
FS <francesco9721@gmail.com>
2022-11-23 15:14:04 +0100

    practise

--- First/src/First/Aufgaben/src/StringK/Aufgabe2.java
+++ First/src/First/Aufgaben/src/StringK/Aufgabe2.java
@@ -6,20 +6,47 @@ public class Aufgabe2 {
 
 	public static void main(String[] args) {
 		
+		
 		// Array Initialisierung
 		int [] iZahlen = new int [5];
 		int iEingabe;
 		int iSumme = 0;
+		String antwort;
+		boolean bKorrekt = true;
+		boolean bFalsch = false;
 		Scanner sc = new Scanner(System.in);
 		
+	
+		
+		// Ausgabe
+		do{
+			System.out.println("Eingabeaufforderung <ja> oder <nein>?");
+		antwort = sc.next();
+		
+		if(antwort.equals("ja")) {
+			bKorrekt = false;
+			bFalsch = false;
+		}
+		else if(antwort.equals("nein")){
+			bKorrekt = false;
+			bFalsch = true;
+		}
+		
+		if(bKorrekt) System.out.println("Falsche Eingabe");;
+		
+		}while(bKorrekt);
+		
 		for (int i = 0; i < iZahlen.length; i++) {
-			System.out.println("Geben sie die " + (i+1) +". Zahl ein:");
+			if(bFalsch == false) {
+				System.out.println("Geben sie die " + (i+1) +". Zahl ein:");
+			}
 			iEingabe = sc.nextInt();
 			iSumme = iSumme + iEingabe;
 		}
 		System.out.println("Gesamtsumme: " + iSumme);
 		sc.close();
-
+		}
+	
 	}
 
-}
+
